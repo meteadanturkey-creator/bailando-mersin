136e3a243e5f69eb737e35cce7a454fb3af5dd4c
meteadanturkey-creator <meteadanturkey@gmail.com>
2026-02-03 00:29:27 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="tr">
+      <head>
+        <title>Bailando Music & Dance Bar | Latin Club Mersin</title>
+        <meta
+          name="description"
+          content="Mersin Yenişehir'de Latin dans, canlı müzik ve gece eğlencesi. Bailando Music & Dance Bar."
+        />
+      </head>
+      <body className="bg-black text-white">{children}</body>
+    </html>
+  );
+}
